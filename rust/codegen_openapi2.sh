495a1367510be66a9409d7b56a4b632a64aeefef
Bryan Newbold <bnewbold@robocracy.org>
2018-05-25 14:01:10 -0700

    Object => serde_json::Value automation

--- rust/codegen_openapi2.sh
+++ rust/codegen_openapi2.sh
@@ -3,4 +3,9 @@
 cargo swagger fatcat-openapi2.yml fatcat-api --docker-tag=v2.3.1
 sudo chown `whoami`:`whoami` -R fatcat-api
 git checkout fatcat-api/Cargo.toml
+
+# Hack to handle "extra" JSON fields
+sed -i 's/Object/serde_json::Value/g' fatcat-api/src/models.rs
+sed -i 's/extern crate uuid;/extern crate serde_json;\nextern crate uuid;/g' fatcat-api/src/models.rs
+
 cargo fmt
