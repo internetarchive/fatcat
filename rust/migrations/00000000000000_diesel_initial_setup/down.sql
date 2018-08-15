0c3ac87be632d1142a5b1c5e6096330d4693bdaa
Bryan Newbold <bnewbold@robocracy.org>
2018-08-14 20:11:02 -0700

    disable base diesel setup (for cockroach)

--- rust/migrations/00000000000000_diesel_initial_setup/down.sql
+++ rust/migrations/00000000000000_diesel_initial_setup/down.sql
@@ -2,5 +2,6 @@
 -- and other internal bookkeeping. This file is safe to edit, any future
 -- changes will be added to existing projects as new migrations.
 
-DROP FUNCTION IF EXISTS diesel_manage_updated_at(_tbl regclass);
-DROP FUNCTION IF EXISTS diesel_set_updated_at();
+SELECT 1;
+-- DROP FUNCTION IF EXISTS diesel_manage_updated_at(_tbl regclass);
+-- DROP FUNCTION IF EXISTS diesel_set_updated_at();
