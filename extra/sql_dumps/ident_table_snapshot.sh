53900e322075f2d2ab35673fd5b6b8a469019bf2
Bryan Newbold <bnewbold@robocracy.org>
2018-09-13 14:47:52 -0700

    improvements to ident_table_snapshot.sh

--- extra/sql_dumps/ident_table_snapshot.sh
+++ extra/sql_dumps/ident_table_snapshot.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+DATABASE_URI="${DATABASE_URI:-fatcat}"
+OUTPUT_DIR="${1:-.}"
 set -e -u -o pipefail
 
-DATESLUG="`date +%Y-%m-%d.%H%M%S`"
-DATABASE="fatcat"
+# This script needs to be run from the directory with the 'dump_idents.sql'
+# script in it.
 
-echo "Running SQL..."
-psql fatcat < ./dump_idents.sql
+# The DATABASE_URI env variable is optional, defaults to 'fatcat' (meaning,
+# local postgres, database named 'fatcat')
+
+# An optional argument is a path to a directory to save output.
+
+DATESLUG="`date +%Y-%m-%d.%H%M%S`"
+
+echo "Will move output to '${OUTPUT_DIR}'"
+echo "Running SQL (from '${DATABASE_URI}')..."
+psql $DATABASE_URI < ./dump_idents.sql
 
 CHANGELOG_REV="`head -n1 /tmp/fatcat_ident_latest_changelog.tsv`"
-OUTFILE="fatcat_idents.$DATESLUG.r$CHANGELOG_REV.tar.xz"
+OUTFILE="${OUTPUT_DIR}/fatcat_idents.$DATESLUG.r$CHANGELOG_REV.tar.xz"
 
 echo "Compressing..."
-tar -C /tmp -c --xz --verbose \
+tar -C /tmp -c --gzip --verbose \
     -f $OUTFILE \
     fatcat_ident_latest_changelog.tsv \
     fatcat_ident_containers.tsv \
