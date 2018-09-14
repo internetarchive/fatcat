6eda33b38ab2a1851f0d30bc4369acf3f4b965d4
Bryan Newbold <bnewbold@robocracy.org>
2018-09-13 17:30:01 -0700

    more agressive gh-pages branch move

--- extra/update_gh-pages.sh
+++ extra/update_gh-pages.sh
@@ -8,7 +8,7 @@ mkdir -p /tmp/fatcat-ghpages
 cp -r target/doc/fatcat target/doc/fatcat_api_spec /tmp/fatcat-ghpages
 cd ..
 git checkout gh-pages
-mv /tmp/fatcat-ghpages/* .
+mv -f /tmp/fatcat-ghpages/* .
 git add fatcat fatcat_api_spec
 git commit -m "updating rendered manpage for github docs" || true
 git checkout master
