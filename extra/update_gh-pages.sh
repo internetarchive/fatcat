5cfbe5e5016bfba95764d497b60069914fac53bc
Bryan Newbold <bnewbold@robocracy.org>
2018-09-13 17:34:33 -0700

    tweak gh-pages script yet again

--- extra/update_gh-pages.sh
+++ extra/update_gh-pages.sh
@@ -8,7 +8,10 @@ mkdir -p /tmp/fatcat-ghpages
 cp -r target/doc/fatcat target/doc/fatcat_api_spec /tmp/fatcat-ghpages
 cd ..
 git checkout gh-pages
-mv -f /tmp/fatcat-ghpages/* .
+mv fatcat fatcat.old_docs || true
+mv fatcat_api_spec fatcat_api_spec.old_docs || true
+mv /tmp/fatcat-ghpages/fatcat .
+mv /tmp/fatcat-ghpages/fatcat_api_spec .
 git add fatcat fatcat_api_spec
 git commit -m "updating rendered manpage for github docs" || true
 git checkout master
