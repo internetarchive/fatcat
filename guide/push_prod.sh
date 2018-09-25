94831f130e87286ad7394ec10f8e61d7ecd72adf
Bryan Newbold <bnewbold@robocracy.org>
2018-09-24 18:04:14 -0700

    guide: actually build (not watch) before pushing

--- guide/push_prod.sh
+++ guide/push_prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+mdbook clean
+mdbook build
 rsync -arv book/ fatcat-prod-vm:/srv/fatcat/guide
--- guide/push_qa.sh
+++ guide/push_qa.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+mdbook clean
+mdbook build
 rsync -arv book/ fatcat-qa-vm:/srv/fatcat/guide
