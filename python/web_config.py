ff0053e3652012ee98412780fb6f82b367bf9400
Bryan Newbold <bnewbold@robocracy.org>
2018-11-13 21:15:18 -0800

    load more config from env

--- python/web_config.py
+++ python/web_config.py
@@ -7,9 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     GIT_REVISION = subprocess.check_output(["git", "describe", "--always"]).strip()
     # This is, effectively, the QA/PROD flag
-    FATCAT_DOMAIN = "qa.fatcat.wiki"
-    ELASTIC_BACKEND = "https://search.fatcat.wiki"
-    ELASTIC_INDEX = "fatcat"
+    FATCAT_DOMAIN = os.environ.get("FATCAT_DOMAIN", default="qa.fatcat.wiki")
+    ELASTIC_BACKEND = os.environ.get("ELASTIC_BACKEND", default="https://search.fatcat.wiki")
+    ELASTIC_INDEX = os.environ.get("ELASTIC_INDEX", default="fatcat")
 
     # "Event more verbose" debug options. SECRET_KEY is bogus.
     #SQLALCHEMY_ECHO = True
