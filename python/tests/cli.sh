c07ab480fa63d0e2379d4380ad3d96aa8cb961a3
Bryan Newbold <bnewbold@robocracy.org>
2019-01-22 12:40:47 -0800

    fix issn -> journal-metadata rename

--- python/tests/cli.sh
+++ python/tests/cli.sh
@@ -14,7 +14,7 @@ set -x
 
 ./fatcat_import.py crossref tests/files/crossref-works.2018-01-21.badsample.json tests/files/ISSN-to-ISSN-L.snip.txt
 ./fatcat_import.py orcid tests/files/0000-0001-8254-7103.json
-./fatcat_import.py issn tests/files/journal_extra_metadata.snip.csv
+./fatcat_import.py journal-metadata tests/files/journal_extra_metadata.snip.csv
 ./fatcat_import.py matched tests/files/matched_sample.json
 ./fatcat_import.py matched tests/files/example_matched.json
 ./fatcat_import.py grobid-metadata tests/files/example_grobid_metadata_lines.tsv
