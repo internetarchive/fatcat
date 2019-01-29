1c3d7d7f1bfea647dc5f1a99ad534fb29c8b0974
Bryan Newbold <bnewbold@robocracy.org>
2019-01-28 22:37:45 -0800

    fix title length checks in crossref

--- python/fatcat_tools/importers/crossref.py
+++ python/fatcat_tools/importers/crossref.py
@@ -338,13 +338,13 @@ class CrossrefImporter(EntityImporter):
         original_title = None
         if obj.get('original-title'):
             original_title = clean(obj.get('original-title')[0], force_xml=True)
-            if len(original_title) < 2:
+            if not original_title or len(original_title) < 2:
                 original_title = None
 
         title = None
         if obj.get('title'):
             title = clean(obj.get('title')[0], force_xml=True)
-            if len(title) < 2:
+            if not title or len(title) < 2:
                 # title can't be just a single character
                 return None
 
