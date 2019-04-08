d43ee5f73e1f7d7f6317d1e7dfb1ddd0e510a353
Bryan Newbold <bnewbold@robocracy.org>
2019-04-08 15:04:50 -0700

    bump max message size to ~20 MBytes

--- python/fatcat_tools/harvest/doi_registrars.py
+++ python/fatcat_tools/harvest/doi_registrars.py
@@ -57,6 +57,7 @@ class HarvestCrossrefWorker:
         self.kafka_config = {
             'bootstrap.servers': kafka_hosts,
             'delivery.report.only.error': True,
+            'message.max.bytes': 20000000, # ~20 MBytes; broker is ~50 MBytes
             'default.topic.config': {
                 'request.required.acks': 'all',
             },
