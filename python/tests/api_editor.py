38dc6686c3acf1d3e970e5969da1e893915cf87e
Bryan Newbold <bnewbold@robocracy.org>
2019-05-07 18:51:47 -0700

    very basic editor update test

---
+++ python/tests/api_editor.py
@@ -0,0 +1,24 @@
+
+import json
+import pytest
+import datetime
+from copy import copy
+
+from fatcat_client import *
+from fatcat_client.rest import ApiException
+from fixtures import *
+
+
+def test_editor_update(api):
+
+    editor_id = api.editor_id
+    orig = api.get_editor(editor_id)
+    newer = api.get_editor(editor_id)
+    newer.username = "temp-bogus-username"
+    api.update_editor(editor_id, newer)
+    check = api.get_editor(editor_id)
+    assert check.username != orig.username
+    assert check.editor_id == orig.editor_id
+    api.update_editor(editor_id, orig)
+    check = api.get_editor(editor_id)
+    assert check == orig
