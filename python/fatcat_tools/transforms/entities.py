4b7c3c7b317cf4793f5ba5ad0d96102f103b66a3
Bryan Newbold <bnewbold@robocracy.org>
2019-10-08 15:57:35 -0700

    dict wrapper for entity_from_json()

--- python/fatcat_tools/transforms/entities.py
+++ python/fatcat_tools/transforms/entities.py
@@ -1,5 +1,5 @@
 
-
+import json
 import collections
 from fatcat_openapi_client import ApiClient
 
@@ -21,11 +21,15 @@ def entity_from_json(json_str, entity_type, api_client=None):
     """
     Hack to take advantage of the code-generated deserialization code
 
-    See not on `entity_to_dict()` about api_client argument.
+    See note on `entity_to_dict()` about api_client argument.
     """
     if not api_client:
         api_client = ApiClient()
     thing = collections.namedtuple('Thing', ['data'])
     thing.data = json_str
     return api_client.deserialize(thing, entity_type)
+
+def entity_from_dict(obj, entity_type, api_client=None):
+    json_str = json.dumps(obj)
+    return entity_from_json(json_str, entity_type, api_client=api_client)
 
