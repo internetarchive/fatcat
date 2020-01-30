59912583926077260d99a9bf77a938c2215eb6c8
Bryan Newbold <bnewbold@robocracy.org>
2020-01-30 00:20:34 -0800

    tweak file ES archive.org domain tracking

--- python/fatcat_tools/transforms/elasticsearch.py
+++ python/fatcat_tools/transforms/elasticsearch.py
@@ -505,5 +505,11 @@ def file_to_elasticsearch(entity):
     t['rels'] = list(set([u.rel for u in entity.urls]))
 
     t['in_ia'] = bool('archive.org' in t['domains'])
+    t['in_ia_petabox'] = bool('archive.org' in t['hosts'])
+
+    # ok, but actually remove archive.org hosts, because they make other
+    # aggregations hard and are a waste of storage
+    t['hosts'] = [h for h in t['hosts'] if h not in ('archive.org', 'web.archive.org')]
+    t['domains'] = [h for h in t['hosts'] if h not in ('archive.org')]
 
     return t
