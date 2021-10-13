f150c9f7389afaacde8a60cb933452e062aad9bb
Bryan Newbold <bnewbold@robocracy.org>
2021-10-12 17:32:20 -0700

    python client: bump version in codegen script

--- python_openapi_client/codegen_python_client.sh
+++ python_openapi_client/codegen_python_client.sh
@@ -20,7 +20,7 @@ docker run \
     --input-spec /tmp/swagger/api.yml \
     --output /tmp/swagger/ \
     --package-name=fatcat_openapi_client \
-    -p packageVersion="0.3.3"
+    -p packageVersion="0.4.0"
 
 sudo chown -R `whoami`:`whoami` $OUTPUT
 mkdir -p fatcat_openapi_client
