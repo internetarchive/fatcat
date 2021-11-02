20ce31bdc9c94a35b98369249544c122821a171f
Bryan Newbold <bnewbold@robocracy.org>
2021-11-02 14:59:42 -0700

    remove 'import *' from fatcat_tools (for transforms)

--- python/fatcat_tools/__init__.py
+++ python/fatcat_tools/__init__.py
@@ -1,5 +1,5 @@
 
 from .api_auth import authenticated_api, public_api
 from .fcid import fcid2uuid, uuid2fcid
-from .transforms import *
-from .kafka import simple_kafka_producer, kafka_fail_fast
+from .kafka import kafka_fail_fast, simple_kafka_producer
+from .transforms import entity_from_dict, entity_from_json, entity_to_dict
