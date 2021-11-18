0cec9c1b9dd009a3380a9548598b15094186c7ea
Bryan Newbold <bnewbold@robocracy.org>
2021-11-17 16:12:43 -0800

    ISSN-L dupes check: output all matches

--- extra/checks/check_issnl.sh
+++ extra/checks/check_issnl.sh
@@ -9,7 +9,7 @@ CONTAINER_DUMP=$1
 zcat $CONTAINER_DUMP \
     | jq '[.issnl, .ident] | @tsv' -r \
     | sort -S 4G \
-    | uniq -d -w 9 \
+    | uniq -D -w 9 \
     > issnl_ident.dupes.tsv
 
 wc -l issnl_ident.dupes.tsv >> counts.txt
